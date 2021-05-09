3f64aab3ad7f6d8081c122cb49fa71271ce07403
Halil İbrahim ARAÇ <iboarac@gmail.com>
2021-05-09 16:13:37 +0300

    Projenin başlangıç aşaması

---
+++ Ilknur.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ilknur.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
